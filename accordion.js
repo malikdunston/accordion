6a48f8ac99547eee9295f72ef43d9852855d3da8
Malik Dunston <hello@malikdunston.com>
2021-04-24 18:59:51 -0400

    cool hover animations w/mouseenter/leave

--- accordion.js
+++ accordion.js
@@ -1,35 +1,29 @@
 const accordion = function(data, elem){
 	for(key of Object.keys(data)){
+		let btn = document.createElement("button"), cont = document.createElement("div"), fold = document.createElement("div");
 	// btn
-		let btn = document.createElement("button");
 		btn.innerHTML = key;
 		btn.addEventListener("click", ()=>{
 			btn.parentElement.classList.toggle("open");
-		})
+		});
 	// cont
-		let cont = document.createElement("div");
 		if(typeof data[key] == "string"){
 			cont.innerHTML = data[key];
 		} else if(typeof data[key] == "object"){
+			// start here with the array stuff...
 			accordion(data[key], cont);
 		}
-	// cont, btn -> fold -> elem -> ... -> .accordion
-		let fold = document.createElement("div");
-		fold.classList.add("fold", key);
+	// fold
+		fold.classList.add("fold");
 		fold.appendChild(btn);
 		fold.appendChild(cont);
+		fold.addEventListener("mouseenter", ()=>{
+			fold.classList.add("open");
+		});
+		fold.addEventListener("mouseleave", ()=>{
+			fold.classList.remove("open");
+		})
 		elem.appendChild(fold);
 	}
 	return elem;
 }
-
-sum = document.createElement("article");
-
-let make = function(elem){
-	sumElse = document.createElement("button");
-	sumElse.innerHTML = "button";
-	elem.appendChild(sumElse);
-	return elem;
-}
-
-console.log(make(sum));
