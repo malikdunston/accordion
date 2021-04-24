10357a593e6bc683304498bf9ebf7999d798225d
Malik Dunston <hello@malikdunston.com>
2021-04-24 11:40:41 -0400

    json matches vanilla

--- accordion.js
+++ accordion.js
@@ -1,27 +1,54 @@
 const accordion = function(data, elem){
 	for(key of Object.keys(data)){
-		let button = document.createElement("button");
-		button.innerHTML = key;
-		let wrapper = document.createElement("div");
-		// wrapper.classList.add(key);
-		wrapper.appendChild(button);
+		console.log(data, key, data[key]);
+	// each prop gets a div.fold...
+	// its the wrapper for each prop...
+		let fold = document.createElement("div");
+		fold.classList.add("fold", key);
+	// each fold has to have two children...
+		let btn = document.createElement("button");
+		let cont = document.createElement("div");
+	// button gets key's name for now...
+		btn.innerHTML = key;
+
+	// 	let button = document.createElement("button");
+	// 	button.innerHTML = key;
+	// 	let wrapper = document.createElement("div");
+	// 	// wrapper.classList.add(key);
+	// 	wrapper.appendChild(button);
+
+
+
+
+	// figuring out what to put into the container...
 		switch (typeof data[key]){
 			case "string" : 
-				wrapper.innerHTML = data[key];
+				cont.innerHTML = data[key];
 				break;
 			case "object" : 
-				wrapper.classList.add("fold");
-				accordion(data[key], wrapper);
+	// 			wrapper.classList.add("fold");
+				accordion(data[key], cont);
 				break;
 		}
-		elem.appendChild(wrapper);
+		// elem.appendChild(wrapper);
+		fold.appendChild(btn);
+		fold.appendChild(cont);
+		elem.appendChild(fold);
 	}
 	return elem;
 }
+
+
+
+
+
+
+
 let domElem = accordion(siteData, document.querySelector(".accordion:nth-of-type(1)"));
 document.querySelectorAll(".fold > *:nth-child(1)").forEach(elem => {
 	elem.addEventListener("click", ()=>{
 		elem.parentElement.classList.toggle("open");
 	})
 });
-// console.log(app, document.querySelector("#example"));
+console.log(domElem);
+console.log(document.querySelector(".accordion:nth-of-type(2)"));
--- data.js
+++ data.js
@@ -1,7 +1,6 @@
 const siteData = {
 	quarterbacks: {
-		depth_chart: 
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! </br>"
+		depth_chart: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! </br>"
 	}
 }
 // const siteData = {
