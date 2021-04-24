973bbbbb55a7beba2f793e9cad8423a420f6045e
Malik Dunston <hello@malikdunston.com>
2021-04-24 18:42:10 -0400

    organizing accordian func

--- accordion.js
+++ accordion.js
@@ -1,38 +1,35 @@
 const accordion = function(data, elem){
 	for(key of Object.keys(data)){
-	// wrapper
-		let fold = document.createElement("div");
-		fold.classList.add("fold", key);
-	// each fold has to have two children...
-		let btn = document.createElement("button"), cont = document.createElement("div");
 	// btn
+		let btn = document.createElement("button");
 		btn.innerHTML = key;
 		btn.addEventListener("click", ()=>{
 			btn.parentElement.classList.toggle("open");
 		})
-		fold.appendChild(btn);
 	// cont
+		let cont = document.createElement("div");
 		if(typeof data[key] == "string"){
 			cont.innerHTML = data[key];
 		} else if(typeof data[key] == "object"){
-		// arrays are objects...
 			accordion(data[key], cont);
-			console.log("arrayyyyyyy");
 		}
 	// cont, btn -> fold -> elem -> ... -> .accordion
+		let fold = document.createElement("div");
+		fold.classList.add("fold", key);
+		fold.appendChild(btn);
 		fold.appendChild(cont);
 		elem.appendChild(fold);
 	}
 	return elem;
 }
 
-// let domElem = accordion(siteData, document.querySelector(".accordion:nth-of-type(1)"));
+sum = document.createElement("article");
 
-let sum = document.createElement("article");
 let make = function(elem){
 	sumElse = document.createElement("button");
 	sumElse.innerHTML = "button";
 	elem.appendChild(sumElse);
 	return elem;
 }
+
 console.log(make(sum));
