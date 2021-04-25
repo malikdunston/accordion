b98b4e9b5495442b17d4514a7a30366270f1f103
Malik Dunston <hello@malikdunston.com>
2021-04-25 02:44:02 -0400

    https://relentless.malikdunston.com

--- accordion.js
+++ accordion.js
@@ -1,36 +1,38 @@
 const accordion = function(data, elem){
+	let obj = [];
 	if(typeof data == "string" || typeof data == "number"){
 		elem.innerHTML = data;
-	}else if(typeof data == "object" && !Array.isArray(data)){
-		let obj = [];
+	}else if(typeof data == "object"){
 		Object.keys(data).forEach(key => {
 			obj.push([key, data[key]]);
 		});
-		compile(obj)
-	}else if(Array.isArray(data)){
-		compile(data);
-	}
-	function compile(data){
-		for(item of data){
+		if(Array.isArray(data)){
+			obj = data;
+		}
+		for(item of obj){
 			let btn = document.createElement("button"), cont = document.createElement("div"), fold = document.createElement("div");
 			if (item.length == 2){ // object!
 			// btn
 				btn.innerHTML = item[0];
 				btn.addEventListener("click", ()=>{
-					btn.parentElement.classList.toggle("open");
+					if (btn.parentElement.classList.contains("open")){
+						btn.parentElement.classList.remove("open");
+						cont.style.maxHeight = "0px";
+					}else{
+						btn.parentElement.classList.add("open")
+						cont.style.maxHeight = "500vh";
+						console.log(cont.scrollHeight);
+					}
 				});
 			// cont
+				cont.style.overflow = "hidden";
+				cont.style.maxHeight = "0px";
+				cont.style.margin = "0px";
 				accordion(item[1], cont);
 			// fold
 				fold.appendChild(btn);
 				fold.appendChild(cont);
 				fold.classList.add("fold");
-				fold.addEventListener("mouseenter", ()=>{
-					fold.classList.add("open");
-				});
-				fold.addEventListener("mouseleave", ()=>{
-					fold.classList.remove("open");
-				})
 				elem.appendChild(fold);
 			}else{ // array!
 				accordion(item, cont);
