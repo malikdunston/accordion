e1dca190ad13b6acaa88d843416982f0f8987afe
Malik Dunston <hello@malikdunston.com>
2021-04-24 21:48:46 -0400

    working on array props

--- accordion.js
+++ accordion.js
@@ -1,29 +1,38 @@
 const accordion = function(data, elem){
-	for(key of Object.keys(data)){
-		let btn = document.createElement("button"), cont = document.createElement("div"), fold = document.createElement("div");
-	// btn
-		btn.innerHTML = key;
-		btn.addEventListener("click", ()=>{
-			btn.parentElement.classList.toggle("open");
-		});
-	// cont
-		if(typeof data[key] == "string"){
-			cont.innerHTML = data[key];
-		} else if(typeof data[key] == "object"){
-			// start here with the array stuff...
-			accordion(data[key], cont);
+	if(typeof data == "object"){
+		if (Array.isArray(data)){
+
 		}
-	// fold
-		fold.classList.add("fold");
-		fold.appendChild(btn);
-		fold.appendChild(cont);
-		fold.addEventListener("mouseenter", ()=>{
-			fold.classList.add("open");
-		});
-		fold.addEventListener("mouseleave", ()=>{
-			fold.classList.remove("open");
-		})
-		elem.appendChild(fold);
+		for(key of Object.keys(data)){
+			let btn = document.createElement("button"), cont = document.createElement("div"), fold = document.createElement("div");
+		// btn
+			btn.innerHTML = key;
+			btn.addEventListener("click", ()=>{
+				btn.parentElement.classList.toggle("open");
+			});
+		// cont
+			if(typeof data[key] == "string"){
+				cont.innerHTML = data[key];
+			} else if(typeof data[key] == "object"){
+				// start here with the array stuff...
+				if (Array.isArray(data[key])){
+					console.log(data[key]);
+
+				};
+				accordion(data[key], cont);
+			}
+		// fold
+			fold.appendChild(btn);
+			fold.appendChild(cont);
+			fold.classList.add("fold");
+			fold.addEventListener("mouseenter", ()=>{
+				fold.classList.add("open");
+			});
+			fold.addEventListener("mouseleave", ()=>{
+				fold.classList.remove("open");
+			})
+			elem.appendChild(fold);
+		}
 	}
 	return elem;
 }
