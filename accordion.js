960dae06910371e8a5dad95a6f84663f4ad0f9a7
Malik Dunston <hello@malikdunston.com>
2021-04-24 22:20:45 -0400

    arrays work, no need to add .fold to html strings

--- accordion.js
+++ accordion.js
@@ -1,7 +1,6 @@
 const accordion = function(data, elem){
 	if(typeof data == "string"){
 		elem.innerHTML = data;
-		// return elem;
 	}else if(typeof data == "object" && !Array.isArray(data)){
 		for(key of Object.keys(data)){
 			let btn = document.createElement("button"), cont = document.createElement("div"), fold = document.createElement("div");
@@ -26,24 +25,11 @@ const accordion = function(data, elem){
 		}
 	}else if(Array.isArray(data)){
 		for(item of data){
-			let btn = document.createElement("button"), cont = document.createElement("div"), fold = document.createElement("div");
-		// btn
-			btn.innerHTML = item;
-			btn.addEventListener("click", ()=>{
-				btn.parentElement.classList.toggle("open");
-			});
+			let cont = document.createElement("div"), fold = document.createElement("div");
 		// cont
 			accordion(item, cont);
 		// fold
-			fold.appendChild(btn);
 			fold.appendChild(cont);
-			fold.classList.add("fold");
-			fold.addEventListener("mouseenter", ()=>{
-				fold.classList.add("open");
-			});
-			fold.addEventListener("mouseleave", ()=>{
-				fold.classList.remove("open");
-			})
 			elem.appendChild(fold);
 		}
 	}
