05db0bc3963e85fc8679006db949b56f59058764
Malik Dunston <hello@malikdunston.com>
2021-04-25 00:48:33 -0400

    accordion() -> compile(data)

--- accordion.js
+++ accordion.js
@@ -1,46 +1,41 @@
 const accordion = function(data, elem){
-	let $data = []; // normalize everything into an array...
 	if(typeof data == "string"){
-		$data.push(data);
-		for(item of $data){
-			elem.innerHTML = item;
-		}
+		elem.innerHTML = data;
 	}else if(typeof data == "object" && !Array.isArray(data)){
+		let obj = [];
 		Object.keys(data).forEach(key => {
-			$data.push([key, data[key]]);
+			obj.push([key, data[key]]);
 		});
-		compile($data)
+		compile(obj)
 	}else if(Array.isArray(data)){
-		$data = data;
-		for(item of $data){
-			let cont = document.createElement("div"), fold = document.createElement("div");
-		// cont
-			accordion(item, cont);
-		// fold
-			fold.appendChild(cont);
-			elem.appendChild(fold);
-		}
+		compile(data);
 	}
-	function compile($data){
-		for(item of $data){
+	function compile(data){
+		for(item of data){
 			let btn = document.createElement("button"), cont = document.createElement("div"), fold = document.createElement("div");
-		// btn
-			btn.innerHTML = item[0];
-			btn.addEventListener("click", ()=>{
-				btn.parentElement.classList.toggle("open");
-			});
-		// cont
-			accordion(item[1], cont);
-		// fold
-			fold.appendChild(btn);
+			if (item.length == 2){ // object!
+			// btn
+				btn.innerHTML = item[0];
+				btn.addEventListener("click", ()=>{
+					btn.parentElement.classList.toggle("open");
+				});
+			// cont
+				accordion(item[1], cont);
+			// fold
+				fold.appendChild(btn);
+				fold.appendChild(cont);
+				fold.classList.add("fold");
+				fold.addEventListener("mouseenter", ()=>{
+					fold.classList.add("open");
+				});
+				fold.addEventListener("mouseleave", ()=>{
+					fold.classList.remove("open");
+				})
+				elem.appendChild(fold);
+			}else{ // array!
+				accordion(item, cont);
+			}
 			fold.appendChild(cont);
-			fold.classList.add("fold");
-			fold.addEventListener("mouseenter", ()=>{
-				fold.classList.add("open");
-			});
-			fold.addEventListener("mouseleave", ()=>{
-				fold.classList.remove("open");
-			})
 			elem.appendChild(fold);
 		}
 	}
