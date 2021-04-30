b7761dc8bdcc2ee45b6c4ab0e204b4a28070524b
Malik Dunston <hello@malikdunston.com>
2021-04-30 12:47:28 -0400

    maxheight fix... want to make it variable, not just really big random number.

--- new.js
+++ new.js
@@ -1,47 +1,4 @@
 const accordion = function(data, elem){
-	if(typeof data == "string" || typeof data == "number"){
-		return (elem.innerHTML = data);
-	}else if(typeof data == "object"){
-		if(Array.isArray(data)){
-			var obj = data;
-		} else {
-			var obj = Object.keys(data).map(key => {
-				return [key, data[key]];
-			});
-		}
-		for(item of obj){
-			let fold = document.createElement("div");
-			fold.classList.add("fold");
-			let btn = document.createElement("button");
-			let cont = document.createElement("div");
-			if (item.length == 2){ // object!
-				btn.innerHTML = item[0];
-				btn.addEventListener("click", ()=>{
-					btn.parentElement.classList.toggle("open");
-					if (cont.style.maxHeight !== "0px"){
-						cont.style.maxHeight = "0px";
-					}else{
-						cont.style.maxHeight = "500vh";
-					}
-				});
-			// cont
-				cont.style.cssText = "overflow: hidden; max-height: 0; margin: 0px;"
-				accordion(item[1], cont);
-			// fold
-				// fold.appendChild(cont);
-				elem.appendChild(fold);
-			}else{ // array!
-				accordion(item, cont);
-			}
-				fold.appendChild(btn);
-			fold.appendChild(cont);
-			elem.appendChild(fold);
-		}
-		return elem;
-	}
-}
-
-const old_accordion = function(data, elem){
 	let obj = [];
 	if(typeof data == "string" || typeof data == "number"){
 		elem.innerHTML = data;
