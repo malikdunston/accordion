70cc933636f0e7165ad052f1aa86f1b3684b6031
Malik Dunston <hello@malikdunston.com>
2021-05-03 01:04:27 -0400

    accordion() - shorter, more performant code.

--- accordion.js
+++ accordion.js
@@ -6,24 +6,38 @@ const accordion = function(data, elem){
 		case "object":
 			counter = 0;
 			for([key, value] of Object.entries(data)){
-
-				// info = [counter, x, data[x]];
-				info = [counter, key, value]
-
+				let btn = document.createElement("button"),
+				cont = document.createElement("div"), 
+				fold = document.createElement("div");
 				if(Array.isArray(data)){
-					console.log("array:  ", info);
+					accordion(value, cont);
 				}else{
-					console.log("obj:  ", info);
+				// btn
+					btn.innerHTML = key;
+					btn.addEventListener("click", evnet =>{
+						if (btn.parentElement.classList.contains("open")){
+							cont.style.maxHeight = "0px";
+						}else{
+							cont.style.maxHeight = "500vh";
+						}
+						btn.parentElement.classList.toggle("open");
+					});
+				// cont
+					cont.style.overflow = "hidden";
+					cont.style.maxHeight = "0px";
+					cont.style.margin = "0px";
+					accordion(value, cont);
+				// fold
+					fold.appendChild(btn);
+					fold.appendChild(cont);
+					fold.classList.add("fold");
+					elem.appendChild(fold);
 				}
-				// accordion(data[x], elem)
-
-
-
+				fold.appendChild(cont);
+				elem.appendChild(fold);
 				counter++;
 			}
 	}
-	// log([data, elem]);
-	// console.log([data, elem]);
 	return elem;
 
 
@@ -57,7 +71,7 @@ const accordion = function(data, elem){
 
 
 const old_accordion = function(data, elem){
-	let obj = [];
+	// let obj = [];
 	// if(typeof data == "string" || typeof data == "number"){
 	// 	elem.innerHTML = data;
 	// }else if(typeof data == "object"){
@@ -67,37 +81,37 @@ const old_accordion = function(data, elem){
 		// if(Array.isArray(data)){
 		// 	obj = data;
 		// }
-		for(item of obj){
-			let btn = document.createElement("button"), cont = document.createElement("div"), fold = document.createElement("div");
-			if (item.length == 2){ // object!
+		// for(item of obj){
+			// let btn = document.createElement("button"), cont = document.createElement("div"), fold = document.createElement("div");
+			// if (item.length == 2){ // object!
 			// btn
-				btn.innerHTML = item[0];
-				btn.addEventListener("click", ()=>{
-					if (btn.parentElement.classList.contains("open")){
-						btn.parentElement.classList.remove("open");
-						cont.style.maxHeight = "0px";
-					}else{
-						btn.parentElement.classList.add("open")
-						cont.style.maxHeight = "500vh";
-						console.log(cont.scrollHeight);
-					}
-				});
+				// btn.innerHTML = item[0];
+				// btn.addEventListener("click", ()=>{
+				// 	if (btn.parentElement.classList.contains("open")){
+				// 		btn.parentElement.classList.remove("open");
+				// 		cont.style.maxHeight = "0px";
+				// 	}else{
+				// 		btn.parentElement.classList.add("open")
+				// 		cont.style.maxHeight = "500vh";
+				// 		console.log(cont.scrollHeight);
+				// 	}
+				// });
 			// cont
-				cont.style.overflow = "hidden";
-				cont.style.maxHeight = "0px";
-				cont.style.margin = "0px";
-				accordion(item[1], cont);
+				// cont.style.overflow = "hidden";
+				// cont.style.maxHeight = "0px";
+				// cont.style.margin = "0px";
+				// accordion(item[1], cont);
 			// fold
-				fold.appendChild(btn);
-				fold.appendChild(cont);
-				fold.classList.add("fold");
-				elem.appendChild(fold);
-			}else{ // array!
-				accordion(item, cont);
-			}
-			fold.appendChild(cont);
-			elem.appendChild(fold);
-		}
+				// fold.appendChild(btn);
+				// fold.appendChild(cont);
+				// fold.classList.add("fold");
+				// elem.appendChild(fold);
+			// }else{ // array!
+				// accordion(item, cont);
+			// }
+			// fold.appendChild(cont);
+			// elem.appendChild(fold);
+		// }
 	// }
-	return elem;
+	// return elem;
 }
