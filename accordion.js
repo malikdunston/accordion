2446f7f83d474a4865fc29b48d42e25a7e8e7427
Malik Dunston <hello@malikdunston.com>
2021-05-03 02:32:37 -0400

    findParents() <- btn.onClick

--- accordion.js
+++ accordion.js
@@ -1,34 +1,34 @@
-const accordion = function(data, elem){
-	switch (typeof data){
+const accordion = function (data, elem) {
+	switch (typeof data) {
 		case "string":
 		case "number":
-			return(elem.innerHTML = data);
+			return (elem.innerHTML = data);
 		case "object":
-			counter = 0;
-			for([key, value] of Object.entries(data)){
+			counter = 0; // don't really need this yet.
+			for ([key, value] of Object.entries(data)) {
 				let btn = document.createElement("button"),
-					cont = document.createElement("div"), 
+					cont = document.createElement("div"),
 					fold = document.createElement("div");
-				if(!Array.isArray(data)){
-				// btn
+				if (!Array.isArray(data)) {
+					// btn
 					btn.innerHTML = key;
-					btn.addEventListener("click", event =>{
-						if (cont.style.maxHeight !== "0px"){
-							cont.style.maxHeight = "0px";
-						}else{
+				// maybe add evlist to fold, then use event.target to select btn.
+				// do this to bundle all fold code into a "compile"
+					btn.addEventListener("click", event => {
+						console.log(findParents(btn));
+						cont.style.maxHeight !== "0px" ?
+							cont.style.maxHeight = "0px" :
 							cont.style.maxHeight = "500vh";
-						}
 						fold.classList.toggle("open");
 					});
-				// cont
+					// cont
 					cont.style.overflow = "hidden";
 					cont.style.maxHeight = "0px";
 					cont.style.margin = "0px";
-				// fold
+					// fold
 					fold.appendChild(btn);
 					fold.appendChild(cont);
 					fold.classList.add("fold");
-					elem.appendChild(fold);
 				}
 				accordion(value, cont);
 				fold.appendChild(cont);
@@ -36,5 +36,16 @@ const accordion = function(data, elem){
 				counter++;
 			}
 			return elem;
+	}
+// find all parent elems of an elem...
+// want to make all parents overflow = whatever... 
+// from https://stackoverflow.com/questions/8729193/how-to-get-all-parent-nodes-of-given-element-in-pure-javascript
+	function findParents(e){
+		var els = [];
+		while (e) {
+			els.unshift(e);
+			e = e.parentNode;
+		}
+		return els;
 	}
 }
