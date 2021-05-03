4f4aad32e91c27fb257ea7959b8c180de33d11bf
Malik Dunston <hello@malikdunston.com>
2021-05-03 03:21:17 -0400

    accordion.js: -3 lines, -107 chars, no more switch...

--- accordion.js
+++ accordion.js
@@ -1,26 +1,44 @@
-const accordion = function (data, elem) {
+const accordion =  (data, elem) => {
+// if/else, nested f() = 24 lines of code (909 chars)
+	function iterate() {
+		for ([key, value] of Object.entries(data)) {
+			let btn = document.createElement("button"),
+				cont = document.createElement("div"),
+				fold = document.createElement("div");
+			if (!Array.isArray(data)) {
+				btn.innerHTML = key; btn.classList.add("btn");
+				cont.style.overflow = "hidden";	cont.style.maxHeight = "0px"; cont.style.margin = "0px";
+				fold.appendChild(btn); fold.appendChild(cont); fold.classList.add("fold");
+			}
+			accordion(value, cont);
+			fold.appendChild(cont);
+			fold.addEventListener("click", event => {
+				if(parentsOf(event.target)[0].classList.contains("btn") && cont.style.maxHeight !== "0px" ){
+					cont.style.maxHeight = "0px"
+				}else cont.style.maxHeight = "500vh";
+				fold.classList.toggle("open");
+			});
+			elem.appendChild(fold);
+		}
+	}
+	typeof data == "object" ?
+		iterate() :
+		elem.innerHTML = data;
+	return elem;
+// switch = 27 lines of code (1016 chars)
 	switch (typeof data) {
 		case "string":
 		case "number":
 			return (elem.innerHTML = data);
 		case "object":
-			counter = 0; // don't really need this yet.
 			for ([key, value] of Object.entries(data)) {
 				let btn = document.createElement("button"),
 					cont = document.createElement("div"),
 					fold = document.createElement("div");
 				if (!Array.isArray(data)) {
-				// btn
-					btn.innerHTML = key;
-					btn.classList.add("btn");
-				// cont
-					cont.style.overflow = "hidden";
-					cont.style.maxHeight = "0px";
-					cont.style.margin = "0px";
-				// fold
-					fold.appendChild(btn);
-					fold.appendChild(cont);
-					fold.classList.add("fold");
+					btn.innerHTML = key; btn.classList.add("btn");
+					cont.style.overflow = "hidden";	cont.style.maxHeight = "0px"; cont.style.margin = "0px";
+					fold.appendChild(btn); fold.appendChild(cont); fold.classList.add("fold");
 				}
 				accordion(value, cont);
 				fold.appendChild(cont);
@@ -33,7 +51,6 @@ const accordion = function (data, elem) {
 					}else console.log("you didn't click on a button");
 				});
 				elem.appendChild(fold);
-				counter++;
 			}
 			return elem;
 	}
@@ -43,7 +60,8 @@ const accordion = function (data, elem) {
 			els.push(e);
 			e = e.parentNode;
 		}
-		els.length -= 3; // remove doc, html, body...
+	// remove doc, html, body...
+		els.length -= 3; 
 		return els;
 	}
 }
