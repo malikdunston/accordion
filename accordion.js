a03a1f14dfe65e1d5b95083125bf29f23909238a
Malik Dunston <hello@malikdunston.com>
2021-05-06 22:09:12 -0400

    ajs and vanilla match formatting

--- accordion.js
+++ accordion.js
@@ -1,35 +1,51 @@
-const accordion =  (data, elem) => {
-	(typeof data == "object" && data !== null) ? iterate() : elem.innerHTML = data;
-	return elem;
-	function iterate() {
-		for ([key, value] of Object.entries(data)) {
-			console.log(key);
-			let btn = document.createElement("button"),
-			cont = document.createElement("div"),
-			fold = document.createElement("div");
-			if (!Array.isArray(data)) {
-				btn.innerHTML = key; btn.classList.add("btn");
-				cont.style.overflow = "hidden";	cont.style.maxHeight = "0px"; cont.style.margin = "0px";
-				fold.appendChild(btn); fold.appendChild(cont); fold.classList.add("fold");
+(function () {
+	angular.module("accordion", [])
+	.directive("accordion", function(){
+		return {
+			restrict: 'A',
+			scope: {
+				data: "<"
+			},
+			link: function(s, e, a){
+				let accordion =  (data, elem) => {
+					(typeof data == "object" && data !== null) ? iterate() : elem.innerHTML = data;
+					return elem;
+					function iterate() {
+						for ([key, value] of Object.entries(data)) {
+							let btn = document.createElement("button"),
+							cont = document.createElement("div"),
+							fold = document.createElement("div");
+							if (!Array.isArray(data)) {
+								btn.innerHTML = key; btn.classList.add("btn");
+								cont.style.overflow = "hidden";	cont.style.maxHeight = "0px"; cont.style.margin = "0px";
+								fold.appendChild(btn); fold.appendChild(cont); fold.classList.add("fold");
+							}
+							accordion(value, cont);
+							fold.appendChild(cont);
+							btn.addEventListener("click", () => {
+								fold.classList.toggle("open");
+								cont.style.maxHeight !== "0px" ?
+									cont.style.maxHeight = "0px" :
+									cont.style.maxHeight = "500vh";
+							});
+							elem.appendChild(fold);
+						}
+					}
+					function parentsOf(e){
+						var els = [];
+						while (e) {
+							els.push(e);
+							e = e.parentNode;
+						}
+						els.length -= 3; 
+						return els;
+					}
+				}
+				s.$watch("data", function(newVal, oldVal){
+					var acc = accordion(newVal, document.createElement("div"));
+					e[0].appendChild(acc)
+				})
 			}
-			accordion(value, cont);
-			fold.appendChild(cont);
-			btn.addEventListener("click", () => {
-				fold.classList.toggle("open");
-				cont.style.maxHeight !== "0px" ?
-					cont.style.maxHeight = "0px" :
-					cont.style.maxHeight = "500vh";
-			});
-			elem.appendChild(fold);
 		}
-	}
-	function parentsOf(e){
-		var els = [];
-		while (e) {
-			els.push(e);
-			e = e.parentNode;
-		}
-		els.length -= 3; 
-		return els;
-	}
-}
+	});
+})()		
